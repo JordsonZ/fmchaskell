418cd9fd4a5fb1fc04f7e50c3f5f776350c75369
JordsonZ <jordsongalvao@gmail.com>
2025-09-26 15:06:14 -0300

    add signum

--- Solutions/FMCBabyNat.hs
+++ Solutions/FMCBabyNat.hs
@@ -125,9 +125,11 @@ factorial (S n) = S n * factorial n
 
 -- signum of a number (-1, 0, or 1)
 sg :: Nat -> Nat
-sg = undefined
+sg O = O
+sg (S _) = S O
+-- Nats não possui numeros negativos
 
 -- lo b a is the floor of the logarithm base b of a
 lo :: Nat -> Nat -> Nat
-lo = undefined
+lo = 
 
