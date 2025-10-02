6e143b7f80b89ac7e5032563ca2086b66dad922d
JordsonZ <jordsongalvao@gmail.com>
2025-10-02 17:57:00 -0300

    correcao no FMCBool and add flip, . and .> in FMCFun

--- Solutions/FMCFun.hs
+++ Solutions/FMCFun.hs
@@ -23,9 +23,16 @@ uncurry f(m , n) = f m n
 -- flip takes a (currified) binary function
 -- and returns one that behaves the same but takes its arguments in the opposite order
 
+flip :: (a -> b -> c) -> b -> a -> c 
+flip f n m = f m n
+
 -- (.) takes two composable functions and returns their composition
 
+(.) :: (b -> c) -> (a -> b) -> a -> c
+(f . g) m = f (g m)
+
 -- (.>) is composition but in diagramatic notation (should be ; but Haskell forbids)
+(.>) :: (a -> b) -> (b -> c) -> a -> c
 (.>) = flip (.)
 
 -- ($) takes a function and a suitable argument and applies the function to the argument
